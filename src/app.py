4397593114d5bb5a95c5604d52d1975b52d6e43a
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-05 18:58:10 +0000

    commit

---
+++ src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import json
+app = Flask(__name__)
+
+todos = [{'label': 'My first task', 'done': False},
+         {'label': 'My second task', 'done': False}]
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    todo_tasks = jsonify(todos)
+    return todo_tasks
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = json.loads(request.data)
+    todos.append(request_body)
+    json_text = jsonify(todos)
+
+    return json_text
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    json_text = jsonify(todos)
+    return json_text
+
+    return json_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
